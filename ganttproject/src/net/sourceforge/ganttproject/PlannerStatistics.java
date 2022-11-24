44d32b796d192ffb71440fcd0cfbf238d4a97603
Pedro Inácio <pm.inacio@campus.fct.unl.pt>
2022-11-24 13:41:07 +0000

    Added functions that gather and process the data for the future burndownChart feature in the PlannerStatistics.java

--- ganttproject/src/net/sourceforge/ganttproject/PlannerStatistics.java
+++ ganttproject/src/net/sourceforge/ganttproject/PlannerStatistics.java
@@ -1,12 +1,16 @@
 package net.sourceforge.ganttproject;
 
+import biz.ganttproject.core.time.GanttCalendar;
+import net.sourceforge.ganttproject.task.Task;
 import net.sourceforge.ganttproject.task.TaskManager;
 
 import java.util.Date;
+import java.util.Map;
+import java.util.TreeMap;
 
 /**
  * @author Francisco Parrinha
- *
+ * <p>
  * TODO:
  *      1. Add event listeners to capture the right statistics
  *          - Maybe make observer class (?)
@@ -26,6 +30,8 @@ public class PlannerStatistics {
     private long totalEstimatedTime;
 
     private long currentSpentTime;
+    //
+    private Task[] tasks;
 
     public PlannerStatistics(TaskManager taskManager) {
         Date date = new Date();
@@ -40,6 +46,7 @@ public class PlannerStatistics {
         totalEstimatedTime = countDaysSinceYear(startYear, endYear) +
                 countDaysSinceMonth(startMonth, endMonth, date.getYear());
         finishedTasks = calculateFinishedTasks(taskManager);
+        tasks = taskManager.getTasks();
     }
 
     public int getTotalTasks() {
@@ -111,12 +118,54 @@ public class PlannerStatistics {
     /**
      * TODO
      * <p>
-     * Calculates the number of tasks whose progress is 100½ (finished)
+     * Calculates the number of tasks whose progress is 100% (finished)
      *
      * @param taskManager
      * @return Number of finished tasks
      */
     private int calculateFinishedTasks(TaskManager taskManager) {
-        return 0;
+        int finTasks = 0;
+        for (int currTask = 0; currTask < totalTasks; currTask++)
+            if (tasks[currTask].getCompletionPercentage() == 100)
+                finTasks++;
+
+        return finTasks;
+    }
+
+    /**
+     * List of tasks done at certain date
+     *
+     * @return a treeMap with the KEY as the tasks the end date and a number of tasks completed on that day as the VALUE
+     */
+    private Map<GanttCalendar, Integer> getCompletedCalendar() {
+        //Generates a treeMap with the key as the end date and a number of tasks completed on that day as the value
+        Map<GanttCalendar, Integer> tasksDoneAtDate = new TreeMap<GanttCalendar, Integer>();
+        for (int currTask = 0; currTask < totalTasks; currTask++) {
+            if (tasks[currTask].getCompletionPercentage() == 100) {
+                GanttCalendar currEndDate = tasks[currTask].getEnd();
+                if (!tasksDoneAtDate.containsKey(currEndDate))
+                    tasksDoneAtDate.put(currEndDate, 1);
+                else {
+                    tasksDoneAtDate.put(currEndDate, tasksDoneAtDate.get(currEndDate) + 1);
+                }
+            }
+        }
+        return tasksDoneAtDate;
+    }
+
+    public Map<GanttCalendar, Integer> burndownData() {
+        //Em teoria ordena no putAll, verificar se o ganttCalendar implementa bem o Comparable
+        Map<GanttCalendar, Integer> totalTasksAtDay = new TreeMap<GanttCalendar, Integer>();
+        totalTasksAtDay.putAll(getCompletedCalendar());
+        //
+        Integer pastEntryValue = 0;
+        // Goes through list adding the past value with the current making this list containing the total
+        // tasks done until that day since the beginning (assuming the list is ordered by day (the key))
+        for (Map.Entry<GanttCalendar, Integer> entry : totalTasksAtDay.entrySet()) {
+            Integer currEntryValue = entry.getValue();
+            entry.setValue(pastEntryValue + currEntryValue);
+            pastEntryValue = currEntryValue;
+        }
+        return totalTasksAtDay;
     }
 }
